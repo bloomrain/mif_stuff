73112d252e7c9736ea1a76b0bcd87d8dc44be00e
gege <bloomrain@gmail.com>
2014-05-18 17:23:19 +0300

    Add foreign paragraph tests for aligned paragraph

--- text_aligment/spec/lib/alignment/aligned_paragraph_spec.rb
+++ text_aligment/spec/lib/alignment/aligned_paragraph_spec.rb
@@ -5,12 +5,15 @@ describe Alignment::AlignedParagraph do
     Alignment::AlignedParagraph.new(
       strategy: :blind,
       position: 1,
-      native_paragraphs: initial_native_paragraphs
+      native_paragraphs: initial_native_paragraphs,
+      foreign_paragraphs: initial_foreign_paragraphs
     )
   end
 
   let(:native_paragraph){ Alignment::NativeParagraph.new(position: 1) }
+  let(:foreign_paragraph){ Alignment::ForeignParagraph.new(position: 1) }
   let(:initial_native_paragraphs){ [] }
+  let(:initial_foreign_paragraphs){ [] }
 
   describe "#native_paragraphs=" do
     context "when no paragraphs was setted before" do
@@ -63,5 +66,54 @@ describe Alignment::AlignedParagraph do
     end
   end
 
-  describe "#foreign_paragraphs="
+  describe "#foreign_paragraphs=" do
+    context "when no paragraphs was setted before" do
+      it "sets foreign paragraphs" do
+        expect {
+          subject.foreign_paragraphs = [foreign_paragraph]
+        }.to change {
+          subject.foreign_paragraphs
+        }.from([]).to([foreign_paragraph])
+      end
+
+    it 'sets aligned paragraph for foreign paragraphs' do
+        expect {
+          subject.foreign_paragraphs = [foreign_paragraph]
+        }.to change {
+          foreign_paragraph.aligned_paragraph(:blind)
+        }.from(nil).to(subject)
+      end
+    end
+
+    context "when changing already setted paragraphs" do
+      let(:initial_foreign_paragraph) { Alignment::ForeignParagraph.new(position: 2) }
+      let(:initial_foreign_paragraphs) {
+        [ initial_foreign_paragraph ]
+      }
+
+      it "sets foreign paragraphs" do
+        expect {
+          subject.foreign_paragraphs = [foreign_paragraph]
+        }.to change {
+          subject.foreign_paragraphs
+        }.from([initial_foreign_paragraph]).to([foreign_paragraph])
+      end
+
+      it 'sets aligned paragraph for new foreign paragraphs' do
+        expect {
+          subject.foreign_paragraphs = [foreign_paragraph]
+        }.to change {
+          foreign_paragraph.aligned_paragraph(:blind)
+        }.from(nil).to(subject)
+      end
+
+      it 'removes aligned paragraph for all old foreign paragraphs' do
+        expect {
+          subject.foreign_paragraphs = [foreign_paragraph]
+        }.to change {
+          initial_foreign_paragraph.aligned_paragraph(:blind)
+        }.from(subject).to(nil)
+      end
+    end
+  end
 end
